697cca0232b65d3e6eda970402ef165f8a93a87f
Kran <56488228+kran27@users.noreply.github.com>
2022-10-13 20:26:24 -0700

    Add files
    Added Fallout 1, 2, Tactics sources, .gitignore, and the GOG Launcher (and required DLLs) for FNV

---
+++ Fallout2Launcher/Fallout2.My/MyApplication.cs
@@ -0,0 +1,46 @@
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace Fallout2.My;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+[GeneratedCode("MyTemplate", "10.0.0.0")]
+internal class MyApplication : WindowsFormsApplicationBase
+{
+	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+	[EditorBrowsable(EditorBrowsableState.Advanced)]
+	[DebuggerHidden]
+	[STAThread]
+	internal static void Main(string[] Args)
+	{
+		try
+		{
+			Application.SetCompatibleTextRenderingDefault(WindowsFormsApplicationBase.UseCompatibleTextRendering);
+		}
+		finally
+		{
+		}
+		MyProject.Application.Run(Args);
+	}
+
+	[DebuggerStepThrough]
+	public MyApplication()
+		: base(AuthenticationMode.Windows)
+	{
+		IsSingleInstance = true;
+		EnableVisualStyles = true;
+		SaveMySettingsOnExit = true;
+		ShutdownStyle = ShutdownMode.AfterMainFormCloses;
+	}
+
+	[DebuggerStepThrough]
+	protected override void OnCreateMainForm()
+	{
+		MainForm = MyProject.Forms.English;
+	}
+}

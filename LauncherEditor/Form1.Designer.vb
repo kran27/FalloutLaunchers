94f047eddc6e55aa9063b978c9211fb6c155b4d0
Kran <56488228+kran27@users.noreply.github.com>
2022-10-14 22:33:29 -0700

    Add LauncherEditor
    A tool to modify the images in the fallout 3, new vegas, and 4 launchers.

---
+++ LauncherEditor/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SelectLauncher = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SelectLauncher
+        '
+        Me.SelectLauncher.Location = New System.Drawing.Point(12, 12)
+        Me.SelectLauncher.Name = "SelectLauncher"
+        Me.SelectLauncher.Size = New System.Drawing.Size(182, 23)
+        Me.SelectLauncher.TabIndex = 0
+        Me.SelectLauncher.Text = "Select Launcher"
+        Me.SelectLauncher.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Enabled = False
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Fallout 3", "Fallout: New Vegas", "Fallout 4"})
+        Me.ComboBox1.Location = New System.Drawing.Point(73, 41)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Launcher:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Enabled = False
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 68)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(182, 77)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Image Modification"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(6, 48)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(170, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Replace Images"
+        Me.ToolTip1.SetToolTip(Me.Button2, "Replaces images in the launcher")
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(170, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Extract Images"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(206, 157)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.SelectLauncher)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Launcher Editor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SelectLauncher As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ LauncherEditor/Form1.vb
@@ -0,0 +1,109 @@
+﻿Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar
+
+Public Class Form1
+    Private FilePath As String
+    Private Sub SelectLauncherC(sender As Object, e As EventArgs) Handles SelectLauncher.Click
+        Dim ofd As New OpenFileDialog With {
+            .Filter = "Fallout 3/NV/4 Launcher|*.exe",
+            .Multiselect = False
+        }
+        If ofd.ShowDialog() = DialogResult.OK Then
+            FilePath = ofd.FileName
+            Dim filename = ofd.FileName.Split("\").Last
+            Select Case filename
+                Case "FalloutLauncher.exe", "FalloutLauncherSteam.exe" : ComboBox1.SelectedIndex = 0
+                Case "FalloutNVLauncher.exe" : ComboBox1.SelectedIndex = 1
+                Case "Fallout4Launcher.exe" : ComboBox1.SelectedIndex = 2
+                Case Else : MsgBox("Couldn't identify launcher, please select manually")
+                    ComboBox1.Enabled = True
+            End Select
+            GroupBox1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ComboBox1.Enabled = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not IO.File.Exists("ResourceHacker.exe") Then
+            If MsgBox("Resource Hacker not found, would you like to open the download page?", MsgBoxStyle.YesNo, "Dependency Missing") = DialogResult.Yes Then
+                MsgBox("Download the ZIP version and extract the .exe to this folder")
+                Process.Start("http://www.angusj.com/resourcehacker/#download")
+                Process.Start(Application.StartupPath)
+            Else
+                Application.Exit()
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dir = $"{Application.StartupPath}\{ComboBox1.SelectedItem.Replace(":", "")}"
+        IO.Directory.CreateDirectory(dir)
+        IO.File.Move("ResourceHacker.exe", dir & "\ResourceHacker.exe")
+        Dim rh As New Process With {.StartInfo = New ProcessStartInfo With {.FileName = $"ResourceHacker.exe", .Arguments = "-script scr.txt", .WorkingDirectory = dir}}
+        Dim scrb As New Text.StringBuilder
+        scrb.Append("[FILENAMES]")
+        scrb.Append(vbNewLine)
+        scrb.Append($"Open=""{FilePath}""")
+        scrb.Append(vbNewLine)
+        Select Case ComboBox1.SelectedIndex
+            Case 0, 1 : scrb.Append(My.Resources.FExtract)
+            Case 2 : scrb.Append(My.Resources.F4Extract)
+        End Select
+        IO.File.WriteAllText($"{dir}\scr.txt", scrb.ToString)
+        rh.Start()
+        rh.WaitForExit()
+        IO.File.Move(dir & "\ResourceHacker.exe", "ResourceHacker.exe")
+        IO.File.Delete($"{dir}\ResourceHacker.ini")
+        IO.File.Delete($"{dir}\ResourceHacker.log")
+        IO.File.Delete($"{dir}\scr.txt")
+        MsgBox("Images Extracted")
+        Process.Start(dir)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim dir = $"{Application.StartupPath}\{ComboBox1.SelectedItem.Replace(":", "")}"
+        For Each file As String In IO.Directory.GetFiles(dir)
+            Dim f As New IO.FileInfo(file)
+            If f.Extension = ".bmp" Then
+                Dim bmp = LoadBmp(file)
+                bmp.Save(file, ImageFormat.Bmp)
+            End If
+        Next
+        IO.File.Move("ResourceHacker.exe", dir & "\ResourceHacker.exe")
+        Dim rh As New Process With {.StartInfo = New ProcessStartInfo With {.FileName = $"ResourceHacker.exe", .Arguments = "-script scr.txt", .WorkingDirectory = dir}}
+        Dim scrb As New Text.StringBuilder
+        scrb.Append("[FILENAMES]")
+        scrb.Append(vbNewLine)
+        scrb.Append($"Open=""{FilePath}""")
+        scrb.Append(vbNewLine)
+        scrb.Append($"Save=""{FilePath}""")
+        scrb.Append(vbNewLine)
+        Select Case ComboBox1.SelectedIndex
+            Case 0, 1 : scrb.Append(My.Resources.FReplace)
+            Case 2 : scrb.Append(My.Resources.F4Replace)
+        End Select
+        IO.File.WriteAllText($"{dir}\scr.txt", scrb.ToString)
+        If Not IO.File.Exists(FilePath & ".bak") Then IO.File.Copy(FilePath, FilePath & ".bak")
+        rh.Start()
+        rh.WaitForExit()
+        IO.File.Move(dir & "\ResourceHacker.exe", "ResourceHacker.exe")
+        IO.File.Delete($"{dir}\ResourceHacker.ini")
+        IO.File.Delete($"{dir}\ResourceHacker.log")
+        IO.File.Delete($"{dir}\scr.txt")
+        MsgBox("File Patched")
+    End Sub
+
+    Private Function LoadBmp(file As String) As Bitmap
+        Dim tmp = New Bitmap(file)
+        Dim bmp As New Bitmap(tmp.Width, tmp.Height, PixelFormat.Format24bppRgb)
+        Dim g = Graphics.FromImage(bmp)
+        g.DrawImage(tmp, 0, 0, tmp.Width, tmp.Height)
+        g.Dispose()
+        tmp.Dispose()
+        Return bmp
+    End Function
+End Class
